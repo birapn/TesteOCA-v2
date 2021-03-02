2c95f37146a846bcb88db7d238d0e4a90e026578
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-02 20:34:02 -0300

    Update and rename header.inc to header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
+  <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
+  <title>Análise de Potencial Pessoal - Instruções</title>
+    <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+</head>
+<body>
+<header>
+			<div class="center">
+				<div class="logo left">logoMarca</div>
+			<div class="clear"></div><!-- clear-->
+			</div><!--center-->
+</header>
