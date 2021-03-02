1030f6a2f9788ac2ba3f865247ab2b5ea07619f1
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-02 20:31:15 -0300

    Update and rename footer.inc to footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+
+
+<footer>
+  <p>Nome Empresa</pNome Empresa>
+  <p>Rua/avenida</p>
+  <p>Bairro - número</p>
+  <p>Cidade</p>
+  <h3>Telefone:<span>(19) 99999-9999</span></h3>
+</footer>
+
+<!-- <TABLE cellSpacing=0 cellPadding=0 width=530 bgColor=f0f0f0 border=0 align=center>
+<TBODY><TR><TD vAlign=center align=middle background="" height=100>
+<FONT face=verdana color=gray size=1>
+<p> Dianética Recife<br />
+  Avenida Frei Matias Teves 285<br />
+  Empresarial Graham Bell, Sala 609<br />
+  Ilha do Leite<br />
+  Recife-PE<br />
+<strong>Telefone: (81) 99638-2894</strong></p>
+</FONT>
+</TD></TR></TBODY></TABLE>
+<br><font face=verdana size=1><center><?php echo date('Y'); ?> - Todos os Direitos Reservados à Dianética Recife</center></font> -->
+</body>
+</html>
