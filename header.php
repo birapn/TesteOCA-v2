a85cd0ff809fea452527892acfc1c3cea2eb37b5
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-06 23:07:07 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,25 @@
 <html lang="pt-BR">
 <head>
-  <meta charset="UTF-8">
-  <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
-  <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
-  <title>Análise de Potencial Pessoal - Instruções</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
+    <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Análise de Potencial Pessoal</title>
     <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
 </head>
 <body>
-<header>
-			<div class="center">
-				<div class="logo left">logoMarca</div>
-			<div class="clear"></div><!-- clear-->
-			</div><!--center-->
-</header>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="Link de direcionamento aqui!">
+        <img src="bar.gif" alt="" width="30" height="30" class="d-inline-block align-top">
+        NomeEmpresa
+      </a>
+      <a class="navbar-brand" href="Link de direcionamento aqui!">
+        | Análise de Potencial Pessoal
+      </a>
+    </div>
+  </nav>
+
+  
