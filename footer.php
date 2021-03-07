74bd9e4f7f7852c1a7d055487f78e32572a69851
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-06 23:07:34 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,24 +1,58 @@
+<footer class="bg-dark text-white pt-5 pb-4">
+  <div class="container text-center text-md-left">
+    <div class="row text-center text-md-left">
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">NomeEmpresa</h5>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        s</p>
+        
+      </div>
 
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">UsefulLiks</h5>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Instagram</p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">email@email.com</p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">Endereço-1</p>
+        <p><a href="#" class="text-white" style="text-decoration: none;">(19) 99999-9999</p>
+      </div>
 
-<footer>
-  <p>Nome Empresa</pNome Empresa>
-  <p>Rua/avenida</p>
-  <p>Bairro - número</p>
-  <p>Cidade</p>
-  <h3>Telefone:<span>(19) 99999-9999</span></h3>
+      <hr class="mb-4">
+
+      <div class="row align-itens-center">
+        
+        <div class="col-md-7 col-lg-8">
+          <p> Copyright @2021 Todos direitos reservados:
+            <a href="#" style="text-decoration:none;">
+              <strong class="text-warning">NomeEmpresa</strong>
+            </a>
+          </p>
+        </div>
+
+        <div class="col-md-5 col-lg-4">
+          <div class="text-center text-md-right">
+
+              <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-facebook"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-google-plus"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-youtube"></i></a>
+                </li>             
+              </ul> 
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
 </footer>
-
-<!-- <TABLE cellSpacing=0 cellPadding=0 width=530 bgColor=f0f0f0 border=0 align=center>
-<TBODY><TR><TD vAlign=center align=middle background="" height=100>
-<FONT face=verdana color=gray size=1>
-<p> Dianética Recife<br />
-  Avenida Frei Matias Teves 285<br />
-  Empresarial Graham Bell, Sala 609<br />
-  Ilha do Leite<br />
-  Recife-PE<br />
-<strong>Telefone: (81) 99638-2894</strong></p>
-</FONT>
-</TD></TR></TBODY></TABLE>
-<br><font face=verdana size=1><center><?php echo date('Y'); ?> - Todos os Direitos Reservados à Dianética Recife</center></font> -->
 </body>
 </html>
