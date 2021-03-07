e72563ebc670408ce711fd10158869281c957a0b
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-07 20:02:34 -0300

    Update EntraDados.php

--- EntraDados.php
+++ EntraDados.php
@@ -1,59 +1,64 @@
-<?php
-    $nome = $_POST['nome'];
-    $mail = $_POST['mail'];
-    $sobrenome = $_POST['sobrenome'];
-    $sexo = $_POST['sexo'];
-    $cidade = $_POST['cidade'];
-    $uf = $_POST['uf'];
-    $telefone = $_POST['telefone'];
-    $telefone2 = $_POST['telefone2'];
-    $mail = $_POST['mail'];
-    $conhecipor = $_POST['conhecipor'];
-?>
-
-<br><h2>Dados Pessoais:</h2><br>
-
-<form action='prepara.php' method='post'>
-<table>
-<?php
-if ($nome == null)
-{echo "Nome: ";}
-else 
-{echo "Nome: ";}
-
-echo "
-<input name='nome' value='$nome'required>
-Sobrenome: <input name='sobrenome' value='$sobrenome'>
-	
-
-Idade: <input type=radio value='MAIOR' name='idade' CHECKED>Mais de 18 anos
-<input type=radio value='MENOR' name='idade'>Menos de 18 anos
-	
-
-Sexo: <input type=radio value='MASC' name='sexo' checked>Masculino
-<input type=radio value='FEM' name='sexo'>Feminino
-	
-
-Cidade: <input maxlength='50' size='10' name='cidade' value='$cidade'required>
-UF: <input maxlength='2' size='3' name='uf' value='$uf'required>
-	
-
-Telefone Fixo: <input maxlength='11' size='13' name='telefone' value='$telefone'>;
-Telefone (Celular): <input maxlength='11' size='13' name='telefone2' value='$telefone2'>
-";
-
-if ($mail == null)
-{echo "E-mail:";}
-else 
-{echo "E-mail:";}
-
-echo "
-<input name='mail' value='$mail'required>
-<br>
-Nos diga na caixa abaixo, de que forma ficou conhecendo o teste?
-<p>(Exemplo: google, facebook, instagram, indicação de um amigo, panfleto, etc.)
-<br></br>
-<textarea name='conhecipor' value='$conhecipor'></textarea><br><br>
-<input type=submit value='Enviar dados'>
-";
-?>
+<div class="container-sm text-center" style="margin-top: 5%; margin-bottom: 10%;">
+    <div>
+        <h1 id="titulo">Análise de Potencial Pessoal</h2>
+        <p id="subtitulo">Complete com suas informações.</p>
+    </div>
+    <form action='teste.php' method='post'>
+        <div class="input-group">
+            <span class="input-group-text">Nome e sobrenome:</span>
+            <input type="text" aria-label="First name" class="form-control" name='nome' placeholder="Ex:'João'" required>
+            <input type="text" aria-label="Last name" class="form-control" name='sobrenome' placeholder="Ex:'Silva'" required>
+        </div>
+        <!---->
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-check form-check-inline">
+                    <h4 class=" fs-4 fw-bold">Idade</h4>
+                    <input class="form-check-input" type="radio" name="idade" value="MENOR" >
+                    <label class="form-check-label" for="idade">
+                        Menos de 18 anos
+                    </label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="idade" value="MAIOR" checked>
+                    <label class="form-check-label" for="idade">
+                        Mais de 18 anos
+                    </label>
+                </div>
+            </div>
+            <!---->
+            <div class="col-md-6">
+                <div class="form-check form-check-inline">
+                    <h4 class=" fs-4 fw-bold">Sexo</h4>
+                    <input class="form-check-input" type="radio" name="sexo" value="MASC">
+                    <label class="form-check-label">Masculino</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="sexo"  value="FEM">
+                    <label class="form-check-label">Feminino</label>
+                </div>
+            </div>
+        </div>        
+        <!---->
+        <div class="input-group mb-3">
+            <span class="input-group-text">Cidade:</span>
+            <input type="text" class="form-control" placeholder="Ex:São Paulo" name="cidade" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">Estado:</span>
+            <input type="text" class="form-control" placeholder="Ex:SP" name="uf" maxlength="2" aria-describedby="basic-addon1">
+        </div>
+        <!---->
+        <div class="input-group mb-3">
+            <span class="input-group-text">Telefone:</span>
+            <input type="text" class="form-control" placeholder="São Paulo" name="telefone" maxlength="11" aria-describedby="basic-addon1">
+        </div>
+        <!---->
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">E-mail:</span>
+            <input type="mail" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" name="mail" required>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+</div>
