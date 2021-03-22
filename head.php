3a9d5beac22cdf623a147ebe7bd2f7ba22fc1dcc
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-22 07:40:57 -0300

    Create head.php

---
+++ head.php
@@ -0,0 +1,13 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
+    <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Análise de Potencial Pessoal</title>
+    <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <!--Links aqui embaixo!-->
+
+</head>
