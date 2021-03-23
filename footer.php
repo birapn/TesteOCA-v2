62d6c3637247e8d5c506fd944a3605f697018392
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-23 16:39:38 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,48 +1,51 @@
-<footer class="bg-dark text-white pt-5 pb-4">
-  <div class="container text-center text-md-left">
-    <div class="row text-center text-md-left">
-      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">NomeEmpresa</h5>
-        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        s</p>
+<?php 
+
+echo"
+<footer class='bg-dark text-white pt-5 pb-4'>
+  <div class='container text-center text-md-left'>
+    <div class='row text-center text-md-left'>
+      <div class='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+        <h5 class='text-uppercase mb-4 font-weight-bold text-warning'>$Titulo1</h5>
+        <p>$texto
+        </p>
         
       </div>
 
-      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">UsefulLiks</h5>
-        <p><a href="#" class="text-white" style="text-decoration: none;">Instagram</p>
-        <p><a href="#" class="text-white" style="text-decoration: none;">email@email.com</p>
-        <p><a href="#" class="text-white" style="text-decoration: none;">Endereço-1</p>
-        <p><a href="#" class="text-white" style="text-decoration: none;">(19) 99999-9999</p>
+      <div class='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+        <h5 class='text-uppercase mb-4 font-weight-bold text-warning'>$Titulo2</h5>
+        <p><a href='#' class='text-white' style='text-decoration: none;'>$Linha1</p>
+        <p><a href='#' class='text-white' style='text-decoration: none;'>$Linha2</p>
+        <p><a href='#' class='text-white' style='text-decoration: none;'>$Linha3</p>
+        <p><a href='#' class='text-white' style='text-decoration: none;'>$Linha4</p>
       </div>
 
-      <hr class="mb-4">
+      <hr class='mb-4'>
 
-      <div class="row align-itens-center">
+      <div class='row align-itens-center'>
         
-        <div class="col-md-7 col-lg-8">
+        <div class='col-md-7 col-lg-8'>
           <p> Copyright @2021 Todos direitos reservados:
-            <a href="#" style="text-decoration:none;">
-              <strong class="text-warning">NomeEmpresa</strong>
+            <a href='$Link_Site' style='text-decoration:none;'>
+              <strong class='text-warning'>$nome_empresa</strong>
             </a>
           </p>
         </div>
 
-        <div class="col-md-5 col-lg-4">
-          <div class="text-center text-md-right">
+        <div class='col-md-5 col-lg-4'>
+          <div class='text-center text-md-right'>
 
-              <ul class="list-unstyled list-inline">
-                <li class="list-inline-item">
-                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-facebook"></i></a>
+              <ul class='list-unstyled list-inline'>
+                <li class='list-inline-item'>
+                  <a href='$Facebook' class='btn-floating btn-sm text-white' style='font-size:23px'><i class='fab fa-facebook'></i></a>
                 </li>
-                <li class="list-inline-item">
-                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-google-plus"></i></a>
+                <li class='list-inline-item'>
+                  <a href='$Google' class='btn-floating btn-sm text-white' style='font-size:23px'><i class='fab fa-google-plus'></i></a>
                 </li>
-                <li class="list-inline-item">
-                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-linkedin-in"></i></a>
+                <li class='list-inline-item'>
+                  <a href='$Linkedin' class='btn-floating btn-sm text-white' style='font-size:23px'><i class='fab fa-linkedin-in'></i></a>
                 </li>
-                <li class="list-inline-item">
-                  <a href="link aqui" class="btn-floating btn-sm text-white" style="font-size:23px"><i class="fab fa-youtube"></i></a>
+                <li class='list-inline-item'>
+                  <a href='$Youtube' class='btn-floating btn-sm text-white' style='font-size:23px'><i class='fab fa-youtube'></i></a>
                 </li>             
               </ul> 
 
@@ -56,3 +59,5 @@
 </footer>
 </body>
 </html>
+";
+?>
