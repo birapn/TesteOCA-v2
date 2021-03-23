cd73365df3e29b27cb8a168f9afb329dff721f91
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-23 16:40:02 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,25 +1,21 @@
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
-    <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Análise de Potencial Pessoal</title>
-    <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-</head>
-<body>
-  <nav class="navbar navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="Link de direcionamento aqui!">
-        <img src="bar.gif" alt="" width="30" height="30" class="d-inline-block align-top">
-        NomeEmpresa
-      </a>
-      <a class="navbar-brand" href="Link de direcionamento aqui!">
-        | Análise de Potencial Pessoal
-      </a>
-    </div>
-  </nav>
+<?php
+
+echo"
+  <!--Header - Front-end-->
+    <body>
+      <nav class='navbar navbar-dark bg-dark'>
+        <div class='container'>
+          <a class='navbar-brand' href='Link de direcionamento aqui!'>
+            <img src='$Logo' alt='' width='30' height='30' class='d-inline-block align-top'>
+            $nome_empresa
+          </a>
+          <a class='navbar-brand' href='Link de direcionamento aqui!'>
+            | $Subtitulo
+          </a>
+        </div>
+      </nav>
+
+  ";
+?>
 
   
