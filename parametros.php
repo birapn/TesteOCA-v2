276afcb6cd9efd6fff17da9b666570a2a73cfd82
Felipe Mingoti <57410580+m1ng0D@users.noreply.github.com>
2021-03-26 11:04:43 -0300

    Update parametros.php

--- parametros.php
+++ parametros.php
@@ -1,18 +1,18 @@
 <?php
 //Geral
 $Link_Site="https://cdadianetica.com.br/";
-
-//trocar o nome da imagem
 $Logo = "cancel.jpg";
 
-
 //Redes Sociais
-//adiciona o link para as redes dentro dos "".
 $Facebook="";
 $Google="";
 $Linkedin="";
 $Youtube="";
 
+//analitcs
+//Adiciona-se a tag inteira do analitcs!
+$analitcs="";
+$analitcs2="";
 // Header
 $nome_empresa="Dianética";
 $Subtitulo="Análise de Potencial Pessoal";
@@ -28,20 +28,4 @@ $Linha2 = "Instagram";
 $Linha3 = "Endereço";
 $Linha4 = "Endereço 2";
 
-echo'
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="keywords" content="psicologia, teste, teste de, teste, analise, análise, análise de potencial pessoal, psicologo, psicólogo">
-    <meta name="description" content="Site brasileiro de Análise de Potencial Pessoal!!">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Análise de Potencial Pessoal</title>
-    <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-    <!-----------------------Links aqui embaixo!-------------------------->
-    <link rel="" href="">
-    <link rel="" href="">
-</head>
-';
 ?>
